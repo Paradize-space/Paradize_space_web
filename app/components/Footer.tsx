4a187629495d85451eb38abcd05f7b757483d5f9
Ujjwal <ujjwalverma3115@gmail.com>
2025-09-07 20:33:07 +0530

    footer added

---
+++ app/components/Footer.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Twitter } from 'lucide-react';
+
+const Footer = () => {
+  return (
+    <footer className="bg-black text-white py-4 ">
+      <div className="container mx-auto flex justify-between items-center">
+        <p>&copy; 2025 paradize.space. All rights reserved.</p>
+        <a href="https://x.com/paradize_space" target="_blank" rel="noopener noreferrer">
+          <Twitter className="h-6 w-6 transition-transform duration-300 hover:scale-125" />
+        </a>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
