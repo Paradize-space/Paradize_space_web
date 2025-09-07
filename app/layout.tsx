f0858897a011fdb200c83971b706d765f3381651
Ujjwal Verma <111216510+Ujjwal3115@users.noreply.github.com>
2025-09-07 20:37:13 +0530

    footer added (#4)

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,8 @@ import { Manrope } from "next/font/google";
 import { SpeedInsights } from "@vercel/speed-insights/next";
 import { Analytics } from "@vercel/analytics/next";
 import "./globals.css";
+import Footer from "./components/Footer";
+
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -41,6 +43,7 @@ export default function RootLayout({
         {children}
         <SpeedInsights />
         <Analytics />
+        <Footer />
       </body>
     </html>
   );
