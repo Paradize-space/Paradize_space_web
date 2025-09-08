80f61aec0bfe54f777868581eb8003d5cf535a0d
Manas Thakur <94138492+Manas-thakur@users.noreply.github.com>
2025-09-08 22:33:25 +0530

    Add padding to Footer component

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -3,8 +3,8 @@ import { Twitter } from 'lucide-react';
 
 const Footer = () => {
   return (
-    <footer className="bg-black text-white py-4 ">
-      <div className="container mx-auto flex justify-between items-center">
+    <footer className="bg-black text-white py-4 px-4">
+      <div className="container mx-auto flex justify-between items-center xl:text-lg md:text-md sm:text-xs">
         <p>&copy; 2025 paradize.space. All rights reserved.</p>
         <a href="https://x.com/paradize_space" target="_blank" rel="noopener noreferrer">
           <Twitter className="h-6 w-6 transition-transform duration-300 hover:scale-125" />
